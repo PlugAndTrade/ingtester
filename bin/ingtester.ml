fe97fb8451434c134c5788ebfcde124994c6389f
Simon Thörnqvist <simon@plug-n-trade.com>
2019-02-17 20:46:47 +0100

    add cli

--- bin/ingtester.ml
+++ bin/ingtester.ml
@@ -1,3 +1,33 @@
-let () =
-  (* Lwt_main.run (Doctest.Case.run ~file: "./ingress.yaml") *)
-  Lwt_main.run (Doctest.Case.run_dir "./")
+open Cmdliner
+
+let help =
+  [ `S Manpage.s_common_options
+  ; `P "These options are common to all commands."
+  ; `S "MORE HELP"
+  ; `P "Use `$(mname) $(i,COMMAND) --help' for help on a single command."
+  ; `Noblank
+  ; `S Manpage.s_bugs
+  ; `P "Check bug reports at https://github.com/plugandtrade/ingtester." ]
+
+let default_cmd =
+  let doc = "Kubernetes Ingress testing" in
+  let sdocs = Manpage.s_common_options in
+  let exits = Term.default_exits in
+  let man = help in
+  ( (let open Term in
+    ret (const (`Help (`Pager, None))))
+  , Term.info "ingtester" ~version:"0.0" ~doc ~sdocs ~exits ~man )
+
+let run dir =
+  print_endline (Format.sprintf "GOT DIR %s" dir);
+  Lwt_main.run (Doctest.run_dir dir)
+
+let cmd =
+  let dir =
+    let doc = "run test in directory" in
+    Arg.(value & (opt string "./" & (info ["d"; "dir"] ~docv:"DIR" ~doc)))
+  in Term.(const run $ dir) , Term.info "test" ~doc:"run test doc" ~sdocs:Manpage.s_common_options ~exits:Term.default_exits ~man:help
+
+let cmds = [cmd]
+
+let () = Term.(exit @@ eval_choice default_cmd cmds)
